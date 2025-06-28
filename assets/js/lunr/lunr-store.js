8cf39e58edcfb95a9dc1535ed25f248ce20595ce
purnima215 <purnimat07@gmail.com>
2025-06-28 21:48:36 +0530

    Deploy latest build

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "About",
+        "excerpt":"A more detailed bio can go here.  ","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Books",
+        "excerpt":"   English Books   తెలుగు పుస్తకాలు                          Bandaged Moments (2025)        Mental Health stories by Women Writers of India                                            The BWW Bangalore Anthology (2022)        Another literary experiment in time and memory.                    - **అగాధం** (2021)       *అనుభవాల మధ్య అంతరాల కథలు*   - *ఇంకా రాయాల్సినవి...*   ✅ Telugu book content  ","categories": [],
+        "tags": [],
+        "url": "/books/",
+        "teaser": null
+      },{
+        "title": "Contact",
+        "excerpt":"Provide your email, social links, or embed a form.  ","categories": [],
+        "tags": [],
+        "url": "/contact/",
+        "teaser": null
+      },{
+        "title": "Events",
+        "excerpt":"Upcoming and past literary events.  ","categories": [],
+        "tags": [],
+        "url": "/events/",
+        "teaser": null
+      },{
+        "title": "Media",
+        "excerpt":"Articles, interviews, podcasts, etc.  ","categories": [],
+        "tags": [],
+        "url": "/media/",
+        "teaser": null
+      },]
